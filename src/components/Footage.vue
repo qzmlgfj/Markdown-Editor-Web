87b5b3e9c122d92b81a010a42b1287b0e858d435
qzmlgfj <qzmlgfj@gmail.com>
2021-08-04 23:17:45 +0800

    更新彩蛋位置

--- src/components/Footage.vue
+++ src/components/Footage.vue
@@ -1,26 +1,33 @@
 <template>
     <n-divider />
-    <n-p>{{info}}</n-p>
+    <n-popover trigger="hover" placement="top">
+        <template #trigger>
+            <n-p>{{ info }}</n-p>
+        </template>
+        <n-p>根本不会编译原理和Web只会便乘组件的作者事屑</n-p>
+        <n-p>我自裁罢（绝望</n-p>
+    </n-popover>
 </template>
 
 <script>
-import { NDivider, NP } from "naive-ui";
+import { NDivider, NP, NPopover } from "naive-ui";
 export default {
     name: "Footage",
     components: {
         NDivider,
-        NP
+        NP,
+        NPopover,
     },
-    data(){
-        return{
-            info:"Designed by Ant"
-        }
-    }
+    data() {
+        return {
+            info: "Designed by Ant",
+        };
+    },
 };
 </script>
 
 <style scoped>
-.n-p{
+.n-p {
     text-align: center;
 }
 </style>
--- src/components/Info.vue
+++ src/components/Info.vue
@@ -1,25 +1,17 @@
 <template>
     <n-h1>欢迎！</n-h1>
-    <n-popover trigger="hover" placement="bottom-start">
-        <template #trigger>
-            <n-h2>这是一个简易MarkDown渲染器</n-h2>
-        </template>
-        <n-p>根本不会编译原理和Web只会便乘组件的作者事屑</n-p>
-        <n-p>我自裁罢（绝望</n-p>
-    </n-popover>
+    <n-h2>这是一个简易MarkDown渲染器</n-h2>
     <n-h3>编辑完成后可使用浏览器的打印功能导出pdf</n-h3>
 </template>
 
 <script>
-import { NH1, NH2, NH3, NP, NPopover } from "naive-ui";
+import { NH1, NH2, NH3 } from "naive-ui";
 export default {
     name: "Info",
     components: {
         NH1,
         NH2,
         NH3,
-        NP,
-        NPopover,
     },
 };
 </script>
